310a50e134ea524d8eaab8cdbd7522f3a40371ea
Lucas Silva <lucasmsilva1503@gmail.com>
2021-01-08 01:59:52 +0000

    Funcionario Construct
    Foi criada a class Funcionário com os seus atributos e construtor, foi ainda criado um "override ToString" - Sujeito a Mudança

---
+++ Supermarket/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+    class Funcionario
+    {
+        public string nome;
+        public int codFuncionário;
+        public string dataNascimento;
+
+        public Funcionario(string nome, int codFuncionário, string dataNascimento)
+        {
+            this.nome = nome;
+            this.codFuncionário = codFuncionário;
+            this.dataNascimento = dataNascimento;
+        }
+
+        public override string ToString()
+        {
+            return "| Funcionário: " + nome + " | |Número: " 
+                + codFuncionário + " | |Data Nascimento: " 
+                + dataNascimento + " |";
+        }
+    }
+}
---
+++ Supermarket/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario("Lucas Silva", 1503, "15-03-2000");
+
+            Console.WriteLine(f1.ToString());
+        
+        
+        }
+    }
+}
