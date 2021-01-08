df462b26f5d62a6474cf261ec74aaf59d659b700
Lucas Silva <lucasmsilva1503@gmail.com>
2021-01-08 11:38:07 +0000

    Funcionarios Construct
    Criação da class Funcionarios, Lista de Funcionarios

--- Supermarket/Funcionario.cs
+++ Supermarket/Funcionario.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Supermarket
 {
+    [Serializable]
     class Funcionario
     {
+        //===============|Declaração de Variáveis|===============
         public string nome;
         public int codFuncionário;
         public string dataNascimento;
 
+        //===============|Construtores|===============
         public Funcionario(string nome, int codFuncionário, string dataNascimento)
         {
             this.nome = nome;
@@ -17,11 +21,14 @@ namespace Supermarket
             this.dataNascimento = dataNascimento;
         }
 
+        //===============|Método ToString|===============
         public override string ToString()
         {
             return "| Funcionário: " + nome + " | |Número: " 
                 + codFuncionário + " | |Data Nascimento: " 
                 + dataNascimento + " |";
         }
+
+       
     }
 }
---
+++ Supermarket/Funcionarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Supermarket
+{
+    class Funcionarios
+    {
+        public List<Funcionario> listaDeFuncionarios;
+
+        public Funcionarios()
+        {
+            listaDeFuncionarios = new List<Funcionario>();
+        }
+
+        //===============|Guardar Funcionário em Ficheiro|===============
+        
+    }
+}
