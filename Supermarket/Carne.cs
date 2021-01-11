e7ffc9bed4b1ab79470d2a794995289e70c38ccb
Telmo J Moura <telmop1999@gmail.com>
2021-01-11 20:45:11 +0000

    classe XiXInha(carne) :)

---
+++ Supermarket/Carne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+    class Carne : Produto
+    {
+
+        string tipoCarne;
+        //era top ter uma varieavel tipo 1- congelado, 2- outra coisa 
+
+
+        #region Construtor
+
+        public Carne( string tipoCarne, string marca, float preco, bool disponibilidade, int quantidade, bool vegan) : base(marca, preco, disponibilidade, quantidade, vegan)
+        {
+            this.tipoCarne = tipoCarne;
+            this.Marca = marca;
+            this.Preco = preco;
+            Disponibilidade = true;
+            this.Quantidade = quantidade;
+            Vegan = false;
+        }
+
+        #endregion
+
+
+    }
+}
